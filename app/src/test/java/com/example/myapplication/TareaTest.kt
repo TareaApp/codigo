ab6547550df26592a7c88ac3ab9e60f8f7ed0051
GuacamoleMole <104989112+GuacamoleMole@users.noreply.github.com>
2023-03-12 17:53:28 +0100

    TareaTest y dependecias

---
+++ app/src/test/java/com/example/myapplication/TareaTest.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication
+
+import io.mockk.coEvery
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import negocio.Tarea
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class TareaTest {
+
+    @RelaxedMockK
+    val tar = mockk<Tarea>()
+
+
+    @Test
+    fun test_Tarea_Existe() = runBlocking {
+        coEvery { tar.existe() } returns true
+        assertTrue(tar.existe())
+    }
+
+    @Test
+    fun test_No_Tarea_Existe() = runBlocking {
+        coEvery { tar.existe() } returns false
+        assertFalse(tar.existe())
+    }
+
+}
