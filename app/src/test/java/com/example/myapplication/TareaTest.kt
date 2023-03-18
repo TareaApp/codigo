b571c940f4a58560960eeb25bfc14c0427934c51
YeFan <91212919+YeF4n@users.noreply.github.com>
2023-03-18 11:59:42 +0100

    Tarea Test unitario

--- app/src/test/java/com/example/myapplication/TareaTest.kt
+++ app/src/test/java/com/example/myapplication/TareaTest.kt
@@ -1,37 +1,49 @@
 package com.example.myapplication
 
+import integracion.TareaDB
 import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
 import negocio.Tarea
 import org.junit.Assert.*
+import org.junit.Before
 
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
 
 class TareaTest {
 
-    @RelaxedMockK
-    val tar = mockk<Tarea>()
+    val tarDB = mockk<TareaDB>()
 
-    var variableSinUtilizar = 2; // Para prueba de análisis estático
-
+    lateinit var tar : Tarea
+    @Before
+    fun setUp(){
+        tar = Tarea()
+    }
     @Test
     fun test_Tarea_Existe() = runBlocking {
-        coEvery { tar.existe() } returns true
-        assertTrue(tar.existe())
+        coEvery { tarDB.existe(tar) } returns true
+        assertTrue(tarDB.existe(tar))
     }
 
     @Test
     fun test_No_Tarea_Existe() = runBlocking {
-        coEvery { tar.existe() } returns false
-        assertFalse(tar.existe())
+        coEvery { tarDB.existe(tar) } returns false
+        assertFalse(tarDB.existe(tar))
+    }
+    //TODO borrar luego
+    @Test
+    fun test_Tarea_Guardar(){
+        coEvery { tarDB.guardar(tar) } returns true;
+        assertTrue(tarDB.guardar(tar))
     }
 
-
-
-
-
-
-
+    @Test
+    fun test_No_Tarea_Guardar(){
+        coEvery { tarDB.guardar(tar) } returns false;
+        assertFalse(tarDB.guardar(tar))
+    }
 }
