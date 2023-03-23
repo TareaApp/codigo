6e3065b11c67fd1f5395fa4c4b21c8448f074929
LGM03 <gonzalezlidia9@gmail.com>
2023-03-23 17:28:00 +0100

    funcion para planificar y buscar planificación , pendiente de probar

--- app/src/main/java/negocio/Tarea.kt
+++ app/src/main/java/negocio/Tarea.kt
@@ -13,7 +13,6 @@ class Tarea {
     private var minutos: Int = 0
 
     private var fechaPlan : Calendar? = null
-
     private val tDB = TareaDB()
 
     constructor(nombre: String, asignatura: String, hora: Int, minutos: Int, descripcion: String = ""){
@@ -22,11 +21,8 @@ class Tarea {
         this.hora = hora
         this.minutos = minutos
         this.descripcion = descripcion
-
-
     }
-    //Yo quitaria este constructor y haria las planificaciones con los setters para que sea mas facil desde
-    //la vista forms
+
     constructor(nombre: String, asignatura: String, hora: Int, minutos: Int, descripcion: String = "",fechaPlan : Calendar){
         this.nombre = nombre.trim()
         this.asignatura = asignatura.trim()
@@ -66,6 +62,13 @@ class Tarea {
     fun getMinuto(): Int{
         return minutos
     }
+
+    fun setHora(hora : Int){
+        this.hora=hora
+    }
+    fun setMinuto(min: Int){
+        this.minutos=min
+    }
     fun getPlan(): Calendar?{
         return fechaPlan
     }
@@ -73,12 +76,65 @@ class Tarea {
         fechaPlan = cal
     }
 
-    fun setPlanNull(){
-        fechaPlan = null
+    suspend fun setPlanNull(){ //Le da como planificación la primera disponible
+
+        this.setPlan(Calendar.getInstance()) // le damos una planificación
+        var lista = tDB.tareasPosteriores(this)
+
+        for (i in 0 until lista.size-1) {
+            var primera = lista[i].getPlan()!!
+            var segunda = lista[i+1].getPlan()!!
+
+            if(primera.get(Calendar.HOUR)+lista[i].getHora() + (primera.get(Calendar.MINUTE)+lista[i].getMinuto())/60>24) {
+                //si la tarea abarca dos dias, movemos el dia, hora y duración
+                if(primera.get(Calendar.DAY_OF_MONTH) +1 >29 && primera.get(Calendar.MONTH) == 1 || primera.get(Calendar.DAY_OF_MONTH) +1 == 31 && (primera.get(Calendar.MONTH) == 3 || primera.get(Calendar.MONTH) == 5 ||primera.get(Calendar.MONTH) == 8 &&primera.get(Calendar.MONTH) == 10 )) { //135810
+                    if(primera.get(Calendar.MONTH)+1 ==13){
+                        primera.set(Calendar.MONTH,1)
+                        primera.set(Calendar.DAY_OF_MONTH,1)
+                        primera.set(Calendar.YEAR,primera.get(Calendar.YEAR)+1)
+                    }else{
+                        primera.set(Calendar.MONTH,primera.get(Calendar.MONTH)+1)
+                    }
+                }else{
+                    primera.set(Calendar.DAY_OF_MONTH,primera.get(Calendar.DAY_OF_MONTH)+1)
+                }
+                primera.set(Calendar.HOUR, (primera.get(Calendar.HOUR)+lista[i].getHora() + (primera.get(Calendar.MINUTE)+lista[i].getMinuto())/60)-24)
+                lista[i].setHora(0)
+                lista[i].setMinuto(0)
+            }
+               if(primera.get(Calendar.DAY_OF_MONTH)==segunda.get(Calendar.DAY_OF_MONTH) &&  primera.get(Calendar.MONTH)==segunda.get(Calendar.MONTH)){
+                   if(lista[i].getHora()*60+ lista[i].getMinuto()+ this.getHora()*60+this.getMinuto() < (segunda.get(Calendar.HOUR) -primera.get(Calendar.HOUR))*60 + segunda.get(Calendar.MINUTE) -primera.get(Calendar.MINUTE)){
+                       var fecha = primera
+                       fecha.set(primera.get(Calendar.HOUR)+lista[i].getHora(), primera.get(Calendar.MINUTE)+lista[i].getMinuto())
+                       this.setPlan(fecha)
+                       break
+                   }
+               }else{
+                   if(24 - (lista[i].getHora()+ lista[i].getMinuto()/60+ this.getHora()+this.getMinuto()/60) + segunda.get(Calendar.HOUR) + primera.get(Calendar.MINUTE)/60  < this.getHora()+this.getMinuto()){
+                       var fecha = primera
+                       fecha.set(primera.get(Calendar.HOUR)+lista[i].getHora(), primera.get(Calendar.MINUTE)+lista[i].getMinuto())
+                       this.setPlan(fecha)
+                       break
+                   }
+               }
+            }
     }
 
-    fun planificar() : Boolean{
+    suspend fun planificar() : Boolean{
 
+        var lista = tDB.tareasPosteriores(this)
+        for (i in 0 until lista.size-1) {
+            var primera = lista[i].getPlan()
+            var segunda = lista[i + 1].getPlan()
+            if (primera!!.get(Calendar.DAY_OF_MONTH) == Calendar.getInstance()
+                    .get(Calendar.DAY_OF_MONTH) && primera!!.get(Calendar.MONTH) == Calendar.getInstance()
+                    .get(Calendar.MONTH)) {
+                if (lista[i].getHora() * 60 + lista[i].getMinuto() + this.getHora() * 60 + this.getMinuto() >
+                    (segunda!!.get(Calendar.HOUR) - primera!!.get(Calendar.HOUR)) * 60 + segunda!!.get(Calendar.MINUTE) - primera!!.get(Calendar.MINUTE)) {
+                    return false
+                }
+            }
+        }
         return true
     }
 
