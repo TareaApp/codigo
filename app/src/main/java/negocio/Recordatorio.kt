424698affe808e384042ad4d26b0a163741c66a1
YeFan <fye02@ucm.es>
2023-04-13 21:47:36 +0200

    Clase recordatorio en negocio creado, falta funciones

---
+++ app/src/main/java/negocio/Recordatorio.kt
@@ -0,0 +1,11 @@
+package negocio
+
+import java.util.*
+
+class Recordatorio {
+    private lateinit var nombre: String
+    private lateinit var asignatura: String
+    private lateinit var descripcion: String
+
+    private var fechaRecordatorio = Calendar.getInstance()
+}
